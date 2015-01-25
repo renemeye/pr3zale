69374ae0e4bab3030c4dcf9c6110b2baeba9a920
UCD+ | René Meye <rene.meye@ucdplus.com>
2015-01-25 20:06:45 +0100

    Some workflow optimizations

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,11 @@ class OrdersController < ApplicationController
 
   respond_to :html
 
+  rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+    flash[:error] = "You must select one product at least."
+    redirect_to "/products/"
+  end
+
   def create
     @order = Order.new(order_params)
     @order.event = @event
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
-  has_attached_file :image
+  has_attached_file :image, styles: {thumb: "100x100#"}
 
   validates_attachment :image,
   :presence => true,
