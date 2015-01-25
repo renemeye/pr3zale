438f0b9b564dd35501e2cb33ce5a18a23d3c8345
UCD+ | René Meye <rene.meye@ucdplus.com>
2015-01-25 08:22:18 +0100

    Setup for api_token and api_key on postmark
    in order to support multiple postmark api versions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,10 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method   = :postmark
-  config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_TOKEN'] }
+  config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_TOKEN'], :api_key => ENV['POSTMARK_API_KEY'] }
   config.action_mailer.default_url_options = { host: 'prezale.net' }
   config.action_mailer.default_options = {
     :from => "salesdroid@prezale.net"
