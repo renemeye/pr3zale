86ee35447082143d4c0affdd358fe379b4de1fe1
UCD+ | René Meye <rene.meye@ucdplus.com>
2015-02-19 14:19:20 +0100

    in Subfolder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.action_controller.relative_url_root = '/pr3zale'
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
