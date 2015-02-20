fbd62b6c98782d7695982961928e08f0cf3a2b2f
UCD+ | René Meye <rene.meye@ucdplus.com>
2015-02-20 10:50:47 +0100

    Switched to Static bootsrap and added a workaround for rails bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
     :from => ENV['EMAIL_FROM']
   }
 
+  config.assets.prefix = "#{ENV["RAILS_RELATIVE_URL_ROOT"]}/assets"
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
