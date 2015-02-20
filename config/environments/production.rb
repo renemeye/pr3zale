d98b0860f96336d7760b36d31a26861bc1b4945f
UCD+ | René Meye <rene.meye@ucdplus.com>
2015-02-20 19:10:58 +0100

    Amazon S3 with https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   #Paperclip to Amazon S3
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => :https,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
