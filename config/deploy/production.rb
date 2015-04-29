0ff1db8e268521a4a3c7a3c30a2a0f1193425ab4
UCD+ | René Meye <rene.meye@ucdplus.com>
2015-04-29 17:13:55 +0200

    change rights to deploy user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{meye@rene-meye.de}
-role :web, %w{meye@rene-meye.de}
-role :db,  %w{meye@rene-meye.de}
+role :app, %w{deploy@rene-meye.de}
+role :web, %w{deploy@rene-meye.de}
+role :db,  %w{deploy@rene-meye.de}
 
 
 # Extended Server Syntax
